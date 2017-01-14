766710805a96f3e51330f8228ce4bb9705e7a761
Miloš Kozák <milos.kozak@lejmr.com>
2017-01-14 16:55:19 +0100

    Clean before apache is started, i.e., fix of problematic restarts

--- build/develict-5.4/build.sh
+++ build/develict-5.4/build.sh
@@ -2,6 +2,6 @@
 
 docker build -t php-apache:5.2 . \
     && docker rm develict
-docker run -p 80:80 -v /Users/milos/Dropbox/Projekty/Dockerovani/php-apache/legacy/develict:/var/www/html --name=develict php-apache:5.2
+#docker run -p 80:80 -v /Users/milos/Dropbox/Projekty/Dockerovani/php-apache/build/develict-5.4/web/develict.com/htdocs/:/var/www/html --name=develict php-apache:5.2
 
-#docker run -p 80:80 --name=develict php-apache:5.2
+docker run -p 80:80 --name=develict php-apache:5.2
